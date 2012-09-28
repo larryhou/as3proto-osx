2c1cf1fb4f678b8c768a220c12d93c1ed6ddf084
larryhou <larryhou@foxmail.com>
2012-09-28 14:34:23 +0800

    add bash script to generate code

---
+++ protobuf.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# folder with proto files
+PROTO_DIR=proto
+
+# folder for AS3 code saving
+OUTPUT_DIR=output
+
+
+
+
+
+
+
+
+
+# DO NOT MODIFY IT
+# wrap protoc command line
+while [ true ]
+do
+	clear	
+	echo - - - - - - - - - - - - -
+	echo Type proto file name:
+	read PROTO_FILE
+	
+	PROTO_FILE=$PROTO_DIR/$PROTO_FILE
+	echo 
+	
+	# check proto exist
+	if [[ -f $PROTO_FILE ]]
+	then
+		echo -\> $PROTO_FILE
+	else
+		PROTO_FILE=$PROTO_FILE.proto
+	fi
+	
+	# auto add file suffix
+	if [[ -f $PROTO_FILE ]]
+	then
+		echo -\> $PROTO_FILE
+	else
+		echo $PROTO_FILE does not exist, press any key to try again...
+		read pause
+		continue
+	fi
+	
+	# create folder if not exist
+	if [[ -d $OUTPUT_DIR ]]
+	then
+		echo - - - - -
+	else
+		mkdir $OUTPUT_DIR
+		echo -+-+-+-+-
+	fi
+	
+	echo
+	
+	# generate AS3 code from proto
+	protoc --proto_path=$PROTO_DIR --as3_out=$OUTPUT_DIR $PROTO_FILE
+	
+	# enter another loop
+	echo DONE! Press any key to continue...
+	read pause
+done
